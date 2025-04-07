8575ef9faee7f957511066dc962836219d137931
Your Name <you@example.com>
2025-04-07 08:01:42 +0530

    added filees

--- src/components/Field.rs
+++ src/components/Field.rs
@@ -4,12 +4,17 @@ const MAIN_CSS: Asset = asset!("/assets/main.css");
 
 #[component]
 pub fn TextField() -> Element{
-    
-
+    let mut hash_value = use_signal(|| String::from(""));
     rsx!{
         document::Link { rel: "stylesheet", href: MAIN_CSS } 
         h1 { id:"H1",
             "Rust Cracker"
+        }
+        input{id:"inp",
+            placeholder: "Enter the Hash Value", oninput: move |e| hash_value.set(e.value()) 
+        }
+        button{ id:"btn",
+            "Crack the Hash"
         }
     }
 }
